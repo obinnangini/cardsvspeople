45033601af92c53261743907fe68fcc3c5828280
obinnangini <obinnangini@gmail.com>
2014-03-02 18:36:59 -0500

    Removed Login activity for now: Will implement something simpler. Added logger events for activities as well

--- src/com/example/cardsvspeople/GameActivity.java
+++ src/com/example/cardsvspeople/GameActivity.java
@@ -1,44 +1,25 @@
 package com.example.cardsvspeople;
 
-import java.util.jar.Attributes.Name;
-
+import android.net.sip.SipErrorCode;
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 
-public class MenuActivity extends Activity{// implements OnClickListener {
+public class GameActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_menu);
 		String currentactivity = this.getClass().getSimpleName();
 		Log.d("Life cycle notes", currentactivity + " created." );
-		Button startButton = (Button)findViewById(R.id.game_start_button);
-		startButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				//Use an intent to start game Activity, and pass user's game name
-				//Fetch game name from user class, or server
-				String gameName = "";
-				Intent intent = new Intent(MenuActivity.this,GameActivity.class);
-				intent.putExtra("name", gameName);
-				startActivity(intent);
-			}
-		});
+		setContentView(R.layout.activity_game);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu, menu);
+		getMenuInflater().inflate(R.menu.game, menu);
 		return true;
 	}
 	protected void OnStart()
@@ -47,9 +28,10 @@ public class MenuActivity extends Activity{// implements OnClickListener {
 		String currentactivity = this.getClass().getSimpleName();
 		Log.d("Life cycle notes", currentactivity + " started." );
 	}
+	
 	protected void OnPause()
 	{
-		//super.onPause();
+		super.onPause();
 		String currentactivity = this.getClass().getSimpleName();
 		Log.d("Life cycle notes", currentactivity + " paused." );
 	}
@@ -59,15 +41,5 @@ public class MenuActivity extends Activity{// implements OnClickListener {
 		String currentactivity = this.getClass().getSimpleName();
 		Log.d("Life cycle notes", currentactivity + " resumed." );
 	}
-//	@Override
-//	public void onClick(View v) {
-//		
-//		// TODO Auto-generated method stub
-//		if(View.equals()
-//		{
-//			
-//		}
-//		
-//	}
 
 }
--- src/com/example/cardsvspeople/MainActivity.java
+++ src/com/example/cardsvspeople/MainActivity.java
@@ -11,8 +11,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Intent intent = new Intent(this, LoginActivity.class);
-	    startActivity(intent);
+		//Intent intent = new Intent(this, LoginActivity.class);
+	    //startActivity(intent);
 		
 	}
 
