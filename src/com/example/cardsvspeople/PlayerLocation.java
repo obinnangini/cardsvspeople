ba3bfced44f7a841d2ab73ad454c6db8a4c5d5d0
Brandon Mills <mills.brandont@gmail.com>
2014-04-16 10:04:05 -0400

    Organize imports

--- src/com/example/cardsvspeople/PlayerLocation.java
+++ src/com/example/cardsvspeople/PlayerLocation.java
@@ -1,14 +1,9 @@
 package com.example.cardsvspeople;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
--- src/com/example/cardsvspeople/Round.java
+++ src/com/example/cardsvspeople/Round.java
@@ -2,7 +2,6 @@ package com.example.cardsvspeople;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Round 
